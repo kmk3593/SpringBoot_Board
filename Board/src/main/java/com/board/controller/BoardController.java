9c6f9bb4da9402fbc18325b8cd72cadd0cfb6c12
kmk3593 <alsrbs0219@gmail.com>
2022-07-11 15:26:13 +0900

    Create - 게시글 등록 구현

--- Board/src/main/java/com/board/controller/BoardController.java
+++ Board/src/main/java/com/board/controller/BoardController.java
@@ -1,10 +1,14 @@
 package com.board.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.board.domain.BoardDTO;
 import com.board.service.BoardService;
 
 @Controller
@@ -13,8 +17,38 @@ public class BoardController {
 	@Autowired
 	private BoardService boardService;
 
+	// 글쓰기 화면으로 이동.
+	// 이 컨트롤러를 요청한 화면에서 idx라는 name을 가져와서 idx라는 이름으로 쓰겠다
 	@GetMapping(value = "/board/write.do")
-	public String openBoardWrite(Model model) {
+	public String openBoardWrite(@RequestParam(value = "idx", required = false) Long idx, Model model) {
+		if (idx == null) { 	// idx 가 없으면 껍데기 DTO 생성
+			model.addAttribute("board", new BoardDTO());
+		} else {			// idx 가 있으면 해당 idx로 DTO 가져오기
+			BoardDTO board = boardService.getBoardDetail(idx);
+			if (board == null) {	// idx로 조회했는데 가져온 DTO가 null 이면
+				return "redirect:/board/list.do";	// 글쓰기 화면이 아닌 목록화면으로 이동
+			}
+			model.addAttribute("board", board);
+		}
+
 		return "board/write";
-	}
+	}  
+	 
+	@PostMapping(value = "/board/register.do")
+	public String registerBoard(final BoardDTO params) {
+		try {
+			boolean isRegistered = boardService.registerBoard(params);
+			if (isRegistered == false) {
+				// TODO => 게시글 등록에 실패하였다는 메시지를 전달
+			}
+		} catch (DataAccessException e) {
+			// TODO => 데이터베이스 처리 과정에 문제가 발생하였다는 메시지를 전달
+
+		} catch (Exception e) {
+			// TODO => 시스템에 문제가 발생하였다는 메시지를 전달
+		}
+
+		return "redirect:/board/list.do";
+	} 
 }
+
