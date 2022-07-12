c6efcd208822605117bd79d0726ed8f176f5778a
kmk3593 <alsrbs0219@gmail.com>
2022-07-12 11:26:56 +0900

    게시글 삭제 완료

--- Board/src/main/java/com/board/controller/BoardController.java
+++ Board/src/main/java/com/board/controller/BoardController.java
@@ -35,7 +35,8 @@ public class BoardController {
 
 		return "board/write";
 	}
-	 
+	
+	// 게시글 등록
 	@PostMapping(value = "/board/register.do")
 	public String registerBoard(final BoardDTO params) {
 		try {
@@ -52,12 +53,55 @@ public class BoardController {
 
 		return "redirect:/board/list.do";
 	}
+	
+	// 게시글 리스트
 	@GetMapping(value = "/board/list.do")
 	public String openBoardList(Model model) {
 		List<BoardDTO> boardList = boardService.getBoardList();
 		model.addAttribute("boardList", boardList);
  
 		return "board/list";
+	}
+	
+	// 게시글 리스트에서 게시글 선택하여 확인 = 조회
+	@GetMapping(value = "/board/view.do")
+	public String openBoardDetail(@RequestParam(value = "idx", required = false) Long idx, Model model) {
+		if (idx == null) {
+			// TODO => 올바르지 않은 접근이라는 메시지를 전달하고, 게시글 리스트로 리다이렉트
+			return "redirect:/board/list.do";
+		}
+
+		BoardDTO board = boardService.getBoardDetail(idx);
+		if (board == null || "Y".equals(board.getDeleteYn())) {
+			// TODO => 없는 게시글이거나, 이미 삭제된 게시글이라는 메시지를 전달하고, 게시글 리스트로 리다이렉트
+			return "redirect:/board/list.do";
+		}
+		model.addAttribute("board", board);
+
+		return "board/view";
+	}
+	
+	// 게시글 삭제
+	@PostMapping(value = "/board/delete.do")
+	public String deleteBoard(@RequestParam(value = "idx", required = false) Long idx) {
+		if (idx == null) {
+			// TODO => 올바르지 않은 접근이라는 메시지를 전달하고, 게시글 리스트로 리다이렉트
+			return "redirect:/board/list.do";
+		}
+
+		try {
+			boolean isDeleted = boardService.deleteBoard(idx);
+			if (isDeleted == false) {
+				// TODO => 게시글 삭제에 실패하였다는 메시지를 전달
+			}
+		} catch (DataAccessException e) {
+			// TODO => 데이터베이스 처리 과정에 문제가 발생하였다는 메시지를 전달
+
+		} catch (Exception e) {
+			// TODO => 시스템에 문제가 발생하였다는 메시지를 전달
+		}
+
+		return "redirect:/board/list.do";
 	}
 }
 
